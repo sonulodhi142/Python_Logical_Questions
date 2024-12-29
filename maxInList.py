7660f0aaa672f820e2370eec917fddfbd183d1a2
Sonu Lodhi <sp95759201@gmail.com>
2024-12-29 21:58:52 +0530

    Eliminate the duplicate

--- maxInList.py
+++ maxInList.py
@@ -1,3 +1,8 @@
 # creating a List
 numbers = [7,0,8,5,9,4,2,3,5,6,2,8]
 
+#here we eliminate the duplicate numbers from the list
+not_dup = list(set(numbers))
+print("not duplicate number list = ",not_dup)
+
+
