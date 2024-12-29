c1ae05fe7ae2e91eb137b152b3287f16c1cacf01
Sonu Lodhi <sp95759201@gmail.com>
2024-12-29 22:01:04 +0530

    find max and min in list

--- maxInList.py
+++ maxInList.py
@@ -5,4 +5,10 @@ numbers = [7,0,8,5,9,4,2,3,5,6,2,8]
 not_dup = list(set(numbers))
 print("not duplicate number list = ",not_dup)
 
+#here we find the maximum number from the list
+print("the maximum number in list is = ", max(numbers))
+
+#here we find the minimum number from the list
+print("the minimum number in list is = ", min(numbers))
+
 
