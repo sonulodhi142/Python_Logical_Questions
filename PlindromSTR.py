96984e246ecd74cef32dc4c18cb593428d727d93
Sonu Lodhi <sp95759201@gmail.com>
2024-12-31 19:57:52 +0530

    function to find palindrome

---
+++ PlindromSTR.py
@@ -0,0 +1,8 @@
+#That is the funtion which takes a pragraph and find the maximun length palindrom word in the pragraph and max-length not-plindrom word
+
+#Here the function that return the True or false as per the word
+def is_palindrome(word):
+    return word == word[::-1]
+
+
+
