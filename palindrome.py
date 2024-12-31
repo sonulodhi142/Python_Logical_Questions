54ca41938425d3777921ea801e8455af1e236e07
Sonu Lodhi <sp95759201@gmail.com>
2024-12-31 21:46:27 +0530

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+# program to find the palindrome number from the given number
+
+#create variable to ger number
+str = input("enter a num::")
+
+# Reverse the number or string
+palindrom = str[::-1]
+
