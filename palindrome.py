d7258330a812b9922c64d464bfc1b31d844ad91a
Sonu Lodhi <sp95759201@gmail.com>
2024-12-31 21:47:42 +0530

    palindrome

--- palindrome.py
+++ palindrome.py
@@ -6,3 +6,8 @@ str = input("enter a num::")
 # Reverse the number or string
 palindrom = str[::-1]
 
+# condition to check the number is palindrome or note
+if palindrom == str:
+    print(f"{palindrom } is the palindrom")
+else:
+    print(f"{str } is not the palindrom")
