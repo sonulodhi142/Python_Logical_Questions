644c5e7a1aab04a2c71881431b8c678c52559c10
Sonu Lodhi <sp95759201@gmail.com>
2025-01-01 08:54:16 +0530

    loop to print the series

--- fibbonaci.py
+++ fibbonaci.py
@@ -6,3 +6,9 @@ num2 = 1
 # taking input of no. of terms
 terms = int(input("Enter the number of terms::"))
 
+# loop to swap the variable value and print the series
+
+for i in range(1 , terms+1):
+    print(num1)
+    num1, num2 = num2, num1
+    num1 += num2
