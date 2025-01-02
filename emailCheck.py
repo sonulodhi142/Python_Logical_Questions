17165ba8979272e4a726de8c4b9e9221f21bfd49
Sonu Lodhi <sp95759201@gmail.com>
2025-01-02 20:50:25 +0530

    condition check

--- emailCheck.py
+++ emailCheck.py
@@ -7,3 +7,8 @@ email = "test@example.com"
 # make a pattern to check the Gmail pattarn
 pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" 
 
+# condition check
+if re.match(pattern, email): 
+    print("Valid email!") 
+else: 
+    print("Invalid email!")
